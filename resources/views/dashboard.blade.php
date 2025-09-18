7cc259d0666855c9198611acb9046dcfae57b112
Genesis Pinto <genesispinto20@gmail.com>
2025-09-18 14:23:46 -0400

    v3.1 Changes to be committed: modified:   resources/views/dashboard.blade.php modified:   routes/web.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,5 @@
 <x-app-layout>
 
-    <div class= "container">
-
-    </div>
-
-
     <x-slot name="container">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Inicio') }}
